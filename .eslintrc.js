1a43b41421c5ad9c0114c19a17300c68f5977b38
Kaushal <kaushalraok@gmail.com>
2023-10-19 17:26:23 +0530

    Add pre commit hook for prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
-module.exports={
-    extends: ["prettier"]
-}
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-console': 'off',
+  },
+};
